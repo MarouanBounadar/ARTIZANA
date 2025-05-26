e32806ff1322e1316e194be3e73c94ab65cd6df5
Marouan Bounadar <bounadarmarwane@gmail.com>
2025-05-26 08:21:46 +0100

    Fix NAV BAR

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -100,7 +100,7 @@ export function Navbar() {
       {/* Mobile Menu */}
       <div
         className={cn(
-          "fixed inset-0 z-50 flex flex-col bg-black p-6 transition-transform duration-300 ease-in-out lg:hidden",
+          "fixed inset-0 z-50 flex flex-col bg-black text-white p-6 transition-transform duration-300 ease-in-out",
           isMenuOpen ? "translate-x-0" : "-translate-x-full",
         )}
       >
